32f1aeea1241b297210718c03fa37f113e0cc627
MajPaji <sababi@kth.se>
2021-03-04 11:20:58 +0000

    added products app, models and fixtures and loaded data

---
+++ products/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import Product, Category
+
+# Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'product_type',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'display_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
