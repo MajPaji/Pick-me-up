48e2ca32d5413d4d9a9a6994f08713bea5c7590b
MajPaji <sababi@kth.se>
2021-03-10 21:31:03 +0000

    added quantity +/- buttons and js functionality

---
+++ products/static/products/js/products.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+    function handleButtonStatus(itemId) {
+    var currentQty = parseInt($(`#id_qty_${itemId}`).val());
+    var disableValueMinus = currentQty < 2;
+    var disableValuePlus = currentQty > 98;
+    $(`#decrease-qty_${itemId}`).prop('disabled', disableValueMinus);
+    $(`#increase-qty_${itemId}`).prop('disabled', disableValuePlus);
+    }
+
+    $('.increase-qty').click(function(ev) {
+        ev.preventDefault();
+        var currentQty = parseInt($(this).parent().prev().val());
+        $(this).parent().prev().val(currentQty+1);
+        var itemId = $(this).data('item_id');
+        handleButtonStatus(itemId);
+    })
+
+    $('.decrease-qty').click(function(ev) {
+        ev.preventDefault();
+        var currentQty = parseInt($(this).parent().next().val());
+        $(this).parent().next().val(currentQty-1);
+        var itemId = $(this).data('item_id');
+        handleButtonStatus(itemId);
+    })
+
+    $('.qty_input').change(function() {
+        var itemId = $(this).data('item_id');
+        handleButtonStatus(itemId);
+    })
+
+    var allQtys = $('.qty_input');
+    for(var i=0; i < allQtys.length; i++){
+        var itemId=$(allQtys[i]).data('item_id');
+        handleButtonStatus(itemId)
+    }
+
+})
+
