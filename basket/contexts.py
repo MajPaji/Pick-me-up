2f4c197a4a292c645baa00489d6662eb374ab4e6
MajPaji <sababi@kth.se>
2021-03-10 15:44:08 +0000

    updated basket template and add to basket view

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,19 +1,32 @@
+from django.shortcuts import get_object_or_404
 from django.conf import settings
 from decimal import Decimal
-
+from products.models import Product
 
 def basket_contents(request):
 
     basket_items = []
-    total = 0
+    total_cost = 0
     product_count = 0
+    basket = request.session.get('basket', {})
 
-    total_plus_tax = total + Decimal(total * settings.TAX_PERCENTAGE / 100)
+    for item_id, qty in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total_cost += qty * product.price
+        product_count += qty
+        basket_items.append({
+            'item_id': item_id,
+            'qty': qty,
+            'product': product,
+        })
+
+    total_plus_tax = total_cost + Decimal(total_cost * settings.TAX_PERCENTAGE / 100)
 
     context = {
         'basket_items': basket_items,
-        'total': total,
+        'total_cost': total_cost,
         'product_count': product_count,
         'total_plus_tax': total_plus_tax,
+        'TAX_PERCENTAGE': settings.TAX_PERCENTAGE,
     }
     return context
