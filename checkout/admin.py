0c6ca4d2aa5ee2ffa72de28cadca33c80eadd0df
MajPaji <sababi@kth.se>
2021-03-12 19:02:37 +0000

    added signals, admin and forms for checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Receipt, ReceiptLineItem
 
-# Register your models here.
+
+class ReceiptLineItemAdminInline(admin.TabularInline):
+    model = ReceiptLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class ReceiptAdmin(admin.ModelAdmin):
+    inlines = (ReceiptLineItemAdminInline,)
+
+    readonly_fields = ('receipt_number', 'date',
+                       'total_cost', 'total_plus_tax')
+
+    fields = ('receipt_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address',
+              'total_cost', 'total_plus_tax')
+
+    list_display = ('receipt_number', 'date', 'full_name',
+                    'total_cost', 'total_plus_tax')
+
+    ordering = ('-date',)
+
+
+admin.site.register(Receipt, ReceiptAdmin)
---
+++ checkout/signals.py
@@ -0,0 +1,23 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import ReceiptLineItem
+
+
+@receiver(post_save, sender=ReceiptLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+
+    """ 
+    Update total cost on lineitem updated or created
+     """
+
+    instance.receipt.update_total_cost()
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
+
+    """ 
+    Update total cost on lineitem deleted
+     """
+
+    instance.receipt.update_total_cost()
