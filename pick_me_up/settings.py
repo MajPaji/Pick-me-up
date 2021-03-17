81ce8eb499d1dd951da1e81c317cafc847002374
MajPaji <sababi@kth.se>
2021-03-17 17:37:38 +0000

    removed secret key and set debug

--- pick_me_up/settings.py
+++ pick_me_up/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mbhoa+se9beim48df1z#x!^ojqoacj=usd=@7!3ftx-(ad001d'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['pick-me-up-project.herokuapp.com', 'localhost']
 
