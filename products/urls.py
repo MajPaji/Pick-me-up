c2a6d6bdec7cb36a5380c40499ced8f78f84b483
MajPaji <sababi@kth.se>
2021-03-17 12:00:45 +0000

    Admin user- added edit products functionality

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.products_all, name='products'),
-    path('add_product/', views.add_product, name='add_product'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add_product/', views.add_product, name='add_product'),
+    path('edit_product/<product_id>/', views.edit_product, name='edit_product'),
 ]
