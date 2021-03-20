81ba28bfac096b72e290c18d9ffb417507f789e0
MajPaji <sababi@kth.se>
2021-03-20 14:06:06 +0000

    added 400, 403, 404, 500 error templates

--- home/views.py
+++ home/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def error_404(request):
+    return render(request, '404.html')
+
+
 def home(request):
     """ A view to retrun the home page """
 
